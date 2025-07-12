e7bd8ecace3f1ef76304413c567c061842e04528
Asshley <asshleydeanwaynegozar@gmail.com>
2025-07-13 07:43:47 +0800

    feat(del): implement delete button for cart product items

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,16 +1,31 @@
 import { useState, useContext } from 'react';
 import { CartItemContext } from '../App';
 
-
 export default function ProductDetails({
+	id,
 	imageSource,
 	title,
 	price,
 	description,
 }) {
 	const [quantity, setQuantity] = useState(1);
-    const { item, setItem } = useContext(CartItemContext);
-	
+	const { item, setItem, setCart } = useContext(CartItemContext);
+   // Fix cart checkout logic | it is note displaying
+	const addToCart = () => {
+		const productDetails = {
+			productId: id,
+			productImage: imageSource,
+			productTitle: title,
+			productPrice: price,
+			productDescription: description,
+			productQuantity: quantity,
+		};
+		setItem(item + 1);
+		setCart((product) => ([
+			...product, productDetails
+		]));
+	};
+
 	const incrementQuantity = () => {
 		setQuantity(quantity + 1);
 	};
@@ -38,8 +53,41 @@ export default function ProductDetails({
 				<p>{quantity}</p>
 				<button onClick={() => decrementQuantity()}>-</button>
 			</div>
-			<button onClick={() => setItem(item + 1)}>Add to card</button>
+			<button onClick={() => addToCart()}>Add to card</button>
 		</div>
 	);
 }
 
+export function ProductCart({
+	productId,
+	imageSource,
+	title,
+	price,
+	description,
+	quantity,
+}) {
+
+	const { setCart } = useContext(CartItemContext);
+	const { item, setItem } = useContext(CartItemContext);
+
+	const deleteCartItem = () => {
+		setCart((prevCart) => prevCart.filter((product) => product.productId !== productId));
+		setItem(item - 1);
+	};
+
+	return (
+	<div className='cart-product' key={productId}>
+		<img
+			src={imageSource}
+			alt={title}
+			width={50}
+			height={90}
+		/>
+		<h1>{title}</h1>
+		<h2>{price}</h2>
+		<p>{description}</p>
+		<p>Quantity: {quantity}</p>
+		<button onClick={() => deleteCartItem()}>Remove to cart</button>
+	</div>
+	);
+}
