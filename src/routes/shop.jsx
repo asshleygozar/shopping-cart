2a50d9c34407bfb415cb4f860616dd57d40c7e10
Asshley <asshleydeanwaynegozar@gmail.com>
2025-07-12 22:21:29 +0800

    feat: implement api fetcher and product details renderer using product details card component

---
+++ src/routes/shop.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import ProductDetails from '../components/ProductDetails';
+
+export default function Shop() {
+	const [myData, setData] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const fetchDataForProduct = async () => {
+			try {
+				const productData = await getProductResponse(
+					'https://fakestoreapi.com/products'
+				);
+				setData(productData);
+				setError(null);
+			} catch (err) {
+				setError(err);
+				setData(null);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchDataForProduct();
+	}, []);
+
+	return (
+		<section>
+			<h1>Shop</h1>
+			{error && <div>Error loading the page</div>}
+			{loading && <div>Loading...</div>}
+			{myData &&
+				myData.map((product) => (
+					<ProductDetails
+						key={product.id}
+						imageSource={product.image}
+						title={product.title}
+						price={product.price}
+						description={product.description}
+					/>
+				))}
+		</section>
+	);
+}
+
+const getProductResponse = async (url) => {
+	const response = await fetch(url);
+
+	if (!response.ok) {
+		throw new Error(`Error fetching data: Status ${response.status}`);
+	}
+
+	return response.json();
+};
