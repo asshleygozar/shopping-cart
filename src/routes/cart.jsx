a22421d4bd37c454e29d07e5330cd2438b3cc16a
Asshley <asshleydeanwaynegozar@gmail.com>
2025-07-13 15:41:36 +0800

    remove description props

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -1,27 +1,27 @@
 import { useContext } from 'react';
 import { CartItemContext } from '../App';
-import { ProductCart } from '../components/ProductDetails';
+import { ProductCart } from '../components/ProductCard';
+import '../styles/cart.css';
 
 export default function Cart() {
 	const { item, cart } = useContext(CartItemContext);
 
 	return (
-		<section>
-			<h1>Total number of item: {item}</h1>
-			{cart &&
-				cart.map((product) => (
-					<ProductCart
-						key={product.productId}
-						productId={product.productId}
-						imageSource={product.productImage}
-						title={product.productTitle}
-						price={product.productPrice}
-						description={product.productDescription}
-						quantity={product.productQuantity}
-					/>
-				))}
-
-			{console.log(cart)}
+		<section className='cart-page'>
+			<h1 className='cart-header'>Total number of item: {item}</h1>
+			<main className='cart-product-item-container'>
+				{cart &&
+					cart.map((product) => (
+						<ProductCart
+							key={product.productId}
+							productId={product.productId}
+							imageSource={product.productImage}
+							title={product.productTitle}
+							price={product.productPrice}
+							quantity={product.productQuantity}
+						/>
+					))}
+			</main>
 		</section>
 	);
 }
